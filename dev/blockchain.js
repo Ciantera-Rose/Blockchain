d4793432bec9375511eb31ed3fff6402639e8ac7
Ciantera-Rose <ciantera.rose@gmail.com>
2021-02-24 20:25:40 -0500

    add constructor function, add chain to store new and mined blocks in array, add newTransactions to hold created transactions before placed in block and mined, add createNewBlock method and create a newBlock object

---
+++ dev/blockchain.js
@@ -0,0 +1,12 @@
+// function Blockchain() {
+//     this.chain = []
+//     this.newTransasctions = []
+// }
+
+class Blockchain {
+  constructor() {
+    this.chain = [];
+    this.newTransasctions = [];
+  }
+  //...
+}
