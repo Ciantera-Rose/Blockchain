13aa59c7f1ea5b5a2259e53e6c01887d8574d516
Ciantera-Rose <ciantera.rose@gmail.com>
2021-02-28 02:04:12 -0500

    add create instance of blockchain constr funct and test send bitcoin back to called endpoint and get genesis block

--- dev/api.js
+++ dev/api.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/blockchain', function (req, res) {});
+app.get('/blockchain', function (req, res) {
+  res.send(bitcoin);
+  /* Test get genesis block
+
+  {
+"chain": [
+{
+"index": 1,
+"timestamp": 1614495100093,
+"transactions": [],
+"nonce": 100,
+"hash": "0",
+"previousBlockHash": "0"
+}
+],
+"pendingTransactions": []
+}
+
+  */
+});
 
 app.post('/transaction', function (req, res) {
   console.log(req.body);
   res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
+
+  /* Test req.body
+   {
+  amount: 100,
+  sender: '0AHOA00AFNABVSWAID7SA69A',
+  'recipient:': '0H767GVKGLGOY869600Y'
+} 
+*/
 });
 
 app.get('/mine', function (req, res) {});
