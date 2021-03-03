3a2a39ad71fcd11495404d415a8c38282749c5a0
Ciantera-Rose <ciantera.rose@gmail.com>
2021-03-02 20:10:06 -0500

    add build out transaction endpoint to create a new transaction in Blockchain using createNewTransaction method, add pass in params and store in var, add test in postman and hit blockchain endpoint

--- dev/api.js
+++ dev/api.js
@@ -10,43 +10,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/blockchain', function (req, res) {
   res.send(bitcoin);
-  /* Test get genesis block
+});
 
+app.post('/transaction', function (req, res) {
+  const blockIndex = bitcoin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+  res.json({ note: `Transaction will be added in block ${blockIndex}.` });
+});
+// Gets back transaction will be added in block 2, genesis block aleady initiated
+/*
+Test Blockchain endpoint
   {
 "chain": [
 {
 "index": 1,
-"timestamp": 1614495100093,
+"timestamp": 1614732767270,
 "transactions": [],
 "nonce": 100,
 "hash": "0",
 "previousBlockHash": "0"
 }
 ],
-"pendingTransactions": []
+"pendingTransactions": [
+{
+"amount": 300,
+"sender": "0AHOA00AFNABVJHHOF9S6S9YSA",
+"recipient": "AS986V98AYVOA806706707"
+}
+]
 }
 
   */
-});
-
-app.post('/transaction', function (req, res) {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
-
-  /* Test req.body
-   {
-  amount: 100,
-  sender: '0AHOA00AFNABVSWAID7SA69A',
-  'recipient:': '0H767GVKGLGOY869600Y'
-} 
-*/
-});
 
 app.get('/mine', function (req, res) {});
 
 app.listen(3000, function () {
   console.log('listening on port 3000');
 });
+
 // Build out an API that allows you to interact with and add data to the Blockchain
 // Create a server using the Express.js library
 // Build out endpoints to interact with Blockchain
